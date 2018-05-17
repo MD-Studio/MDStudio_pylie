6b10cf452c9631421661749c8060313e19622238
felipeZ <tifonzafel@gmail.com>
2018-05-17 16:11:44 +0200

    fixed extras in setup

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         'numpy', 'pandas', 'statsmodels', 'jsonschema', 'matplotlib',
         'scikit-learn', 'openpyxl'],
-    extra_requirements={
+    extras_require={
         'test': ['coverage']
     },
     include_package_data=True,
